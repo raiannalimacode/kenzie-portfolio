c7a340e8345a3e7732800f5a030fd29aebdb99dc
Raianna Lima <raiannacodelima@gmail.com>
2023-09-11 14:10:35 -0300

    Feat: Update components.

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,7 +1,7 @@
 export const userData = {
   nameUser: "Raianna Lima",
   githubUser: "raiannalimacode",
-  linkedinUser: "raiannalima",
+  linkedinUser: "raianna-lima-0649451ba",
   whatsappNumber: "+5551995197905",
   emailUser: "raiannacodelima@gmail.com",
   instagramUser: "",
